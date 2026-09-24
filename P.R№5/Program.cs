bb3ba590f04c157fee657bfa13429c7dd941b7fc
TinlQ <gameoverdaz@gmail.com>
2026-09-24 03:18:36 +0300

    Add files via upload

---
+++ P.R№5/Program.cs
@@ -0,0 +1,44 @@
+﻿//***************************************************************//
+//* Практическая работа #5                                      *//
+//*Выполнил: Прохорчук К.А, Группа-2ИСП                         *//
+//*Задание: Определить правильность даты, введеной с клавиатуры *//
+//***************************************************************//
+
+using System;
+
+namespace P.R__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.Title = "\t\t\t\tПрактическая работа #5";
+
+            //Решение 
+            int month, number, MaximumDay;
+
+            Console.WriteLine("Здравствуйте!");
+            Console.Write("Пожалуйста, введите месяц(от 1 до 12)\t");
+            month = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Спасибо! А теперь введите число (от 1 до 31)\t");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (month < 1||month > 12)
+            { Console.WriteLine("Ошибка, вы ввели неправильный месяц, попробуйте еще раз"); }
+            else if (number < 1 || number > 31)
+            { Console.WriteLine("Ошибка, вы ввели неправильный день, попробуйте еще раз"); }
+            if (month == 2 )
+            { MaximumDay = 28; } // сколько дней у Февраля (без високосного года)
+            else if (month == 4|| month == 6|| month == 9|| month == 11)
+                { MaximumDay = 30; } // сколько дней Апрель, Июнь, Сентябрь, Ноябрь
+            else { MaximumDay = 31; } // остальные дни
+            if (number > MaximumDay)
+            { Console.WriteLine("Ошибка, в месяце " + month + " только " + MaximumDay + " дней"); }
+            else { Console.WriteLine("Дата корректна " + number + " / " + month); }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P.R№5/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("P.R№5")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P.R№5")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("5d2d3f22-dcd4-49e4-a2ec-c7faa4025b31")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
